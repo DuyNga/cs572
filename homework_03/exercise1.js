fe6e54154d1a4a10e7f5454b3f68224dc580b40e
duynga <loduynga@gmail.com>
2019-01-16 14:34:42 -0600

    refomart source code

--- homework_03/exercise1.js
+++ homework_03/exercise1.js
@@ -1,20 +1,20 @@
 const dns = require('dns');
-const {promisify} = require('util');
+const { promisify } = require('util');
 const resolve4Async = promisify(dns.resolve4);
 
 //// 1
 const w3 = dns.resolve4('www.mum.edu', function (err, addresses) {
     if (err) throw err;
-  console.log('1. ' + addresses);
+    console.log('1. ' + addresses);
 });
 
 //// 2
 resolve4Async('www.mum.edu')
-.then((addresses)=>console.log('2. ' + addresses))
-.catch((err)=>console.log(err));
+    .then((addresses) => console.log('2. ' + addresses))
+    .catch((err) => console.log(err));
 
 //// 3
-async function main(){
+async function main() {
     try {
         const text = await resolve4Async('www.mum.edu');
         console.log('3. ' + text);
