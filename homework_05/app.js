2acd1de566a0155704771d59c9ea26f2183ba225
duynga <loduynga@gmail.com>
2019-01-19 22:04:43 -0600

    add homework_05

---
+++ homework_05/app.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+const express = require('express');
+
+const app = express();
+
+//Using axios to get data from API with Async/Await block
+async function getUsers(req, resp) {
+	try {
+		let users = await axios.get('https://randomuser.me/api/?results=10');
+		resp.status(200);
+		resp.set('Content-Type', 'application/json');
+		//Set cache response by using Cache-Control or Expires
+		//Cache-Control private means this cache on client only, not on any intermediate servers like proxy
+		resp.set('Cache-Control', 'private max-age=3600');
+		//Set Link on header for pagination
+		const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+		const pages = [`<${fullUrl}?page=1>;rel="first"`, 
+						`<${fullUrl}?page=1>;rel="prev"`,
+						`<${fullUrl}?page=1>;rel="next"`,
+						`<${fullUrl}?page=1>;rel="last"`];
+		resp.set('Link', pages.join(','));
+
+		resp.send(users.data);
+		resp.end();
+	} catch (err) {
+		resp.status(500);
+		resp.end();
+	}
+}
+
+//Forward client information (ip) to server even if the server stand behind a proxy
+app.enable('trust proxy');
+//Prevent client from knowing server framework
+app.disable('x-powered-by');
+//Route should be case sensitive and strict
+app.enable('strict routing');
+app.enable('case sensitive routing');
+//Define GET Route /users
+app.get('/users', getUsers);
+app.listen(3000);
