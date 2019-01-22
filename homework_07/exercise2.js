3d245fd47b895aaf2875a92fcb87a272db2b4d1c
duynga <loduynga@gmail.com>
2019-01-22 17:20:05 -0600

    add homework_07

---
+++ homework_07/exercise2.js
@@ -0,0 +1,51 @@
+const MongoClient = require('mongodb').MongoClient;
+const crypto = require('crypto');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const {
+	DB_USER: user,
+	DB_PASS: password,
+	DB_HOST: hostname,
+	DB_NAME: database
+} = process.env;
+const client = new MongoClient(`mongodb://${user}:${password}@${hostname}/${database}`, { useNewUrlParser: true });
+
+const state = {
+	db: null
+}
+
+exports.connect = (callback) => {
+	if(state.db !== null) {
+		console.log('Reusing db connection');
+		return callback(true);
+	} else {
+		client.connect(err => {
+			if (err !== null) {
+				console.log(err);
+				return callback(false);
+			}
+
+			state.db = client.db(database);
+			return callback(true);
+		});
+	}
+}
+
+exports.close = () => {
+	state.db = null;
+	client.close();
+}
+
+exports.getDecryptedMessage = (callback) => {
+	const algorithm = 'aes256'
+	const pass = 'asaadsaad';
+	const collection = state.db.collection('mwa_homework_07');
+	const message = collection.findOne({_id:10}, {_id: 0});
+
+	message.then(data => {
+		const decipher = crypto.createDecipher(algorithm, pass);
+		const decrypted = decipher.update(data.data, 'hex', 'utf8') + decipher.final('utf8');
+		callback(null, decrypted);
+	}).catch(err => callback(err, null));
+}
